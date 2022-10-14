028c090d051b04606c1c8b1f5926258f3da9bb92
godcandidate <godcandidate@gmail.com>
2022-10-14 19:53:20 +0000

    create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+/* more headers goes there */
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 10
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 0; n <= 10 ; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -3,3 +3,4 @@
 2-print_alphabet.c prints all the alphabets in lowercase
 3-print_alphabets.c prints all alphabets in lower case then upper case
 4-print_alphabt.c prints all lower case letters except e and q
+5-print_numbers.c prints numbers from 0 to 10
