fe3c39f73381b4b038f2fc5a19e04e1bb51ff9b0
godcandidate <godcandidate@gmail.com>
2022-10-14 23:52:33 +0000

    create nt i, j;

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/**
+ * main - Prints a random number as negative or positive
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{     
+	int i, j;
+	for (i = 0; i < 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
+			putchar(' ');
+			putchar('0' + j / 10);
+			putchar('0' + j % 10);
+			if (i == 98 && j == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
