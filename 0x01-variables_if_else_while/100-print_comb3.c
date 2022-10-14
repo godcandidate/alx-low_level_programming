dfa8c656c487141b56a64d2c69e46f8e2d442284
godcandidate <godcandidate@gmail.com>
2022-10-14 22:40:16 +0000

    create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int x, y, = '0', z;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (z = y; z <= '9' z++)
+		{
+			if ( n != z)
+			{
+				putchar(n);
+				pytchar(z);
+			if ( i == z )
+				break;
+			else
+			{
+				putchar(',')
+				putchar(' ')
+			}
+		}
+		y++
+        }
+	putchar('\n');
+	return (0);
+
+}
