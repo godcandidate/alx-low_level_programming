647404e8c75a5838aec36d78ad9583dc0a0ac52b
godcandidate <godcandidate@gmail.com>
2022-10-15 00:13:14 +0000

    update 101-print_comb4.c 1.7

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,31 +12,32 @@ int main(void)
 
 {
 	int m, i, j;
-		for (i = '0'; i <= '9'; i++)
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
 		{
-			for (j = '0'; j <= '9'; j++)
+			for (m = '0'; m <= '9'; m++)
 			{
-				for (m = '0'; m <= '9'; m++)
+				if (i < j && j < m)
 				{
-					if (i < j && j < m)
+					putchar(i);
+					putchar(j);
+					putchar(m);
+					if (i == '7' && j == '8' && m == '9')
 					{
-						putchar(i);
-						putchar(j);
-						putchar(m);
-						if (i == '7' && j == '8' && m == '9')
-						{
-							break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
 		}
-		putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 
 }
