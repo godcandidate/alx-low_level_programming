a954e24e5733fcf0ffd3eb08dbc4d60819c11316
godcandidate <godcandidate@gmail.com>
2022-10-18 12:13:08 +0000

    create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "1-main.c"
+#include "_putchar.c"
+
+void print_alphabet_x10(void)
+{
+	char n;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+			_putchar(n);
+		_putchar('\n');
+	}
+}
