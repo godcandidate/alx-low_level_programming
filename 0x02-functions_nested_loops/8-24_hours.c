9a2f96e543d17e1edeceabefdbe1eb517a18e42f
godcandidate <godcandidate@gmail.com>
2022-10-18 22:38:21 +0000

    update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,9 +6,9 @@ void jack_bauer(void)
 {
 	int min, hour;
 
-	for (hour = 0; i <= 23; hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (min = 0; n <= 59; min++)
+		for (min = 0; min <= 59; min++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
