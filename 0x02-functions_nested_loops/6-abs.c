527175df0b5b0f6b82e771b80be3921af3e95f39
godcandidate <godcandidate@gmail.com>
2022-10-18 16:11:32 +0000

    create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *  _abs - checks the absolute value of a number
+ *  @c: The character to check
+ *
+ *  Return: returns the absolute value of the number
+ */
+int _abs(int c)
+{
+	int i;
+
+	i = abs(c);
+	return (i);
+}
