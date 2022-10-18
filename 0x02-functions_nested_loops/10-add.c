9b680b720af4f0816c35128c74d70d8da904087e
godcandidate <godcandidate@gmail.com>
2022-10-18 21:27:26 +0000

    create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  add - adds two numbers
+ *  @x: first number to add
+ *  @y: next number to add
+ *
+ *  Return: returns the addition of two numbers 
+ */
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y
+
+	return (result);
+}
