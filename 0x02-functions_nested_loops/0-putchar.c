9cb85c46256b4290d9cb4b9dfc148fb7ca08ec33
godcandidate <godcandidate@gmail.com>
2022-10-18 10:52:34 +0000

    create 0x02-functions_nested_loops directory, README.md file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/**
+ * main - Prints the _putchar to output
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	write(1, "_putchar\n", 9);
+	return (0);
+
+}
