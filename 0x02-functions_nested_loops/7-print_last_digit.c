b9ac78acb9be40c9551e9171dcaf8c701461efac
godcandidate <godcandidate@gmail.com>
2022-10-18 17:11:02 +0000

    update 7-print_last_digit.c 2.4

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,15 @@
 int print_last_digit(int n)
 {
 	int i = n % 10;
-	int k;
+	int k,b;
 
 	if (i < 0)
 		i = i * -1;
 	if (i != 0)
 	{
 		k = i;
-		i = (i * 10);
-		i = i + k;
+		b = i * 10;
+		i = k + b;
 		_putchar(i);
 	}
 	else
