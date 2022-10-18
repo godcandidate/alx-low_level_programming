6637d3d32bc441fbef6cdaf7b98f5986132c5e6c
godcandidate <godcandidate@gmail.com>
2022-10-18 15:13:21 +0000

    create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *  _islower - checks if a letter is lowercase
+ *  @c: The character to check
+ *
+ *  Return: 1 if the letter is lower case. O otherwise
+ */
+int _islower(int c)
+{
+	int i;
+
+	i = islower(c);
+
+	return i;
+	
+}
