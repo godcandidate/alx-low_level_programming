3ccd7df756adf6cde33a89466b9b951b4938b877
godcandidate <godcandidate@gmail.com>
2022-10-19 00:06:03 +0000

    update 101-natural.c 1.3

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include "main.h"
+void print_sum_multiples(void);
+
 /**
  * print_sum_multiples - prints the sum of 3 and 5 times 1024
  */
@@ -19,4 +20,15 @@ void print_sum_multiples(void)
 		a += 5;
 	}
 	printf("%d\n", sum_3 + sum_5);
+}
+
+/**
+ * main - prints the sum
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_sum_multiples();
+	return (0);
 }
