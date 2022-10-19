71551ed429dd2e500518d3365afb74712f40589b
godcandidate <godcandidate@gmail.com>
2022-10-19 02:57:52 +0000

    update 100-times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,22 +6,27 @@ void print_times_table(int n)
 {
 	int ni, m, p;
 
-	for (ni = 0; ni <= n; n++)
+	if (n < 0 && and n > 15)
+		_putchar(' ')
+	else
 	{
-		_putchar('0');
-		for (m = 1; m <= n; m++)
-		{
-			_putchar(',');
-			_putchar(' ');
-			p = n * m;
-			if (p <= n)
+		for (ni = 0; ni <= n; n++)
+		{	
+			_putchar('0');
+			for (m = 1; m <= n; m++)
+			{
+				_putchar(',');
 				_putchar(' ');
-			else
-				_putchar((p / 10) + '0');
+				p = n * m;
+				if (p <= n)
+					_putchar(' ');
+				else
+					_putchar((p / 10) + '0');
 
-			_putchar((p % 10) + '0');
+				_putchar((p % 10) + '0');
 
-		}
-		_putchar('\n');
+			}
+			_putchar('\n');
+		}	
 	}
 }
