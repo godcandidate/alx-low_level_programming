777775813365eb3a52a092474cd833bba1b0de4c
godcandidate <godcandidate@gmail.com>
2022-10-19 01:54:50 +0000

    create 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void print_first_98_fibonacci(void);
+
+/**
+ * print_first_98_fibonacci - prints the first 50 fibonacci numbers
+ */
+void print_first_98_fibonacci(void)
+{
+	double n1 = 1, n2 = 2, n3;
+	int i;
+
+	printf("1, 2, ");
+
+	for (i = 2; i < 98; ++i)
+	{
+		n3 = n1 + n2;
+		if (i != 49)
+			printf("%g, ", n3);
+		else
+			printf("%g\n", n3);
+		n1 = n2;
+		n2 = n3;
+	}
+}
+
+/**
+ * main - prints the fibonacci numbers
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	print_first_98_fibonacci();
+	return (0);
+}
