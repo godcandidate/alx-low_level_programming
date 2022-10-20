2ef119e0b7f2973e18cd47d78dc7f83bf9abb9e6
godcandidate <godcandidate@gmail.com>
2022-10-20 08:48:54 +0000

    create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  mul - multiply two integers
+ *  @a: first operand
+ *  @b: second operand
+ *
+ *  Return: the results of the multiplication
+ */
+int mul(int a, int b)
+{
+	int results;
+
+	results = a * b;
+	return (results);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,9 @@
 0-isupper.c
 a function that checks for uppercase character
 
+1-isdigit.c
+a function that checks for a digit
+
+2-mul.c
+a function that multiplies two integers
+
