508ed90fe950e3b2d20333a3a3b75d9557e3540f
godcandidate <godcandidate@gmail.com>
2022-10-20 09:51:58 +0000

    create 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  print_square - draws a square
+ *  @size: the size of the square
+ *
+ *  Return: no return
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -22,3 +22,6 @@ a function that draws a straight line in the terminal
 7-print_diagonal.c
 a function that draws a diagonal line on the terminal
 
+8-print_square.c
+a function that prints a square, followed by a new line
+
