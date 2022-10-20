7050e6a1f88a31c9b72110db6a45184449e19026
godcandidate <godcandidate@gmail.com>
2022-10-20 22:04:55 +0000

    update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,9 @@ void fizzbuzz(void)
 
 	while (i < 101)
 	{
-		if (i % 3 == 0)
+		if (i == 100)
+			printf("Buzz");
+		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
