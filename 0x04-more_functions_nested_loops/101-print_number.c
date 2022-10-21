57ba9d255f2ea827aa993f4fbf7adf2f15e26aae
godcandidate <godcandidate@gmail.com>
2022-10-21 22:42:20 +0000

    update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,34 +8,26 @@
  */
 void print_number(int n)
 {
-	int a5, a6, a7, a8, a9;
-	int s5, s6, s7, s8;
+	unsigned int m, d, count;
 
-	a5 = (n / 10000) % 10;
-	s5 = a5;
-	a6 = (n / 1000) % 10;
-	s6 = s5 + a6;
-	a7 = (n / 100) % 10;
-	s7 = s6 + a7;
-	a8 = (n / 10) % 10;
-	s8 = s7 + a8;
-	a9 = n % 10;
 	if (n < 0)
 	{
-		_putchar('-');
-		a5 *= -1;
-		a6 *= -1;
-		a7 *= -1;
-		a8 *= -1;
-		a9 *= -1;
+		_putchar(45);
+		m = n * -1;
 	}
-	if (s5 != 0)
-		_putchar('0' + a5);
-	if (s6 != 0)
-		_putchar('0' + a6);
-	if (s7 != 0)
-		_putchar('0' + a7);
-	if (s8 != 0)
-		_putchar('0' + a8);
-	_putchar('0' + a9);
+	else
+	{
+		m = n;
+	}
+	d = m;
+	count = 1;
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
