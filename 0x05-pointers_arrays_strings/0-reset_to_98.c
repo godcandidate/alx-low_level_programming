bb792e4eeebf0a1d9e9cde6962f2f96b7b8912c8
godcandidate <godcandidate@gmail.com>
2022-10-24 20:51:40 +0000

    create 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *  _isupper - checks if a letter is uppercase
+ *  @*n: pointer to reset 
+ *
+ *  Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
