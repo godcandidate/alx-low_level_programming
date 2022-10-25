a874a050b40f0757185d2b68f19a298e3cb74c97
godcandidate <godcandidate@gmail.com>
2022-10-25 04:44:44 +0000

    create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  _puts - prints elements of a string
+ *  @str: string to check
+ *  Return: always 0
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (*(s + i))
+		_putchar(*(str + i));
+		i++;
+	_putchar('\n');
+}
