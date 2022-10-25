db06947d31b15f93d84a6344594f45cdf45337c1
godcandidate <godcandidate@gmail.com>
2022-10-25 05:32:56 +0000

    create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ *  puts2 - prints the even characters of a string
+ *  @str: string to check
+ *  Return: always 0
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			putchar(str[i]);
+		else
+			continue;
+	}
+	putchar('\n');
+}
