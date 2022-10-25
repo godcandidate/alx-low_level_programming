48bdc1538eb904f14574a59085504df383ebad8b
godcandidate <godcandidate@gmail.com>
2022-10-25 05:04:43 +0000

    create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ *  print_rev - prints a string in reverse order
+ *  @s: string to check
+ *  Return: always 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	while (i > -1)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+
+}
