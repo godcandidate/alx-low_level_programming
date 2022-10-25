3a4df902643968c102f32fabefa5fce34631c89a
godcandidate <godcandidate@gmail.com>
2022-10-25 05:42:03 +0000

    create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  puts_half - prints half characters of a string
+ *  @str: string to check
+ *  Return: always 0
+ */
+void puts_half(char *str)
+{
+	int j, i;
+
+	i = 0;
+	while (*(str + i))
+		i++;
+	j = i / 2;
+	if (i % 2)
+		j += 1;
+	while (j < i)
+	{
+		putchar(*(str + j));
+		j++;
+	}
+	putchar('\n');
+}
