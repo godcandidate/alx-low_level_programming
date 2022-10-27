73502b10c8c2dbdb1d695a41a632bca730faae0f
godcandidate <godcandidate@gmail.com>
2022-10-27 08:30:49 +0000

    update 1-strncat.c 1.2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,21 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0, j = 0 ;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
 
