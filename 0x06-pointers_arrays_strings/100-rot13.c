151c85addaaff630dbf0b24048b1a21b304d118f
godcandidate <godcandidate@gmail.com>
2022-10-27 14:00:59 +0000

    cta

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes in ROT13
+ * @s: string to encode
+ * Return: returns encoded string.
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char *x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *y = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; x[j] != '\0'; j++)
+		{
+			if (s[i] == x[j])
+			{
+				s[i] = y[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
