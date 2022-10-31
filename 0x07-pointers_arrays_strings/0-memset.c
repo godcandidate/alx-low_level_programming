586aec2d7b9bfa5763bb10d0188c375f6492ad60
godcandidate <godcandidate@gmail.com>
2022-10-31 19:51:11 +0000

    update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,10 +11,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i, k;
+	unsigned int i;
 
-	k = n;
-	for (i = 0; i < k; i++)
-		s[k] = b;
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
 	return (s);
 }
