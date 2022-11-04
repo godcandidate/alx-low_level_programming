acaf9e5707f10af58fd871ea7a0bde95ff7d5e4a
godcandidate <godcandidate@gmail.com>
2022-11-04 09:31:49 +0000

    create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints the results of two numbers 
+ *
+ * Return: Always 0.
+ */
+
+int main (int argc, char *argv[])
+{
+	int result = 0, a, b;
+
+	if (argc == 3)
+	{
+		a = atoi (argv[1]);
+		b = atoi (argv[2]);
+		result = a * b;
+		printf("%d\n", result);
+	}
+
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
