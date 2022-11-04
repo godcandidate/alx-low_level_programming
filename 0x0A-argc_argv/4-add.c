d667b40b56a23b90a3429575015adb45800ae99b
godcandidate <godcandidate@gmail.com>
2022-11-04 11:10:53 +0000

    create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - adds infinite number of arguements
+ *
+ * Return: Always 0.
+ */
+
+int main (int argc, char *argv[])
+{
+	int result = 0, i = 1;
+	int a;
+
+	if (argc < 1)
+	{
+		printf("0\n");
+	}
+
+	else
+	{
+		while (i < argc)
+		{
+			a = atoi (argv[i]);
+			if (a != 0)
+				result += a;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+			i++;
+		}
+		printf("%d\n", result);
+	}
+
+	return (0);
+}
+
