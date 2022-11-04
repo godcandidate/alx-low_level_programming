2d878282645c287c900eee8c2c7b747b019e0e62
godcandidate <godcandidate@gmail.com>
2022-11-04 20:57:34 +0000

    update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - prints the results of two numbers 
- * argc - number of arguements
- * argv - an array that holds the arguements
+ * main - prints the results of two numbers
+ * @argc: number of arguements
+ * @argv: an array that holds the arguements
  *
  * Return: Always 0.
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int result = 0, a, b;
 
 	if (argc == 3)
 	{
-		a = atoi (argv[1]);
-		b = atoi (argv[2]);
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
 		result = a * b;
 		printf("%d\n", result);
 	}
