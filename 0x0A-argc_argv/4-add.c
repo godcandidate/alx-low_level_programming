da274e8f8e74b00390ea176292504420e559adf8
godcandidate <godcandidate@gmail.com>
2022-11-04 21:15:43 +0000

    update 4-add.c 1.1

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,36 +1,34 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-/**
- * main - adds infinite number of arguements
- * @argc: number of arguements
- * @argv: an array that holds the arguements
- *
- * Return: Always 0.
- */
+#include <stdio.h>
 
+/**
+ * main - adds positive numbers
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
 int main(int argc, char *argv[])
 {
-	int result = 0, i, j;
+	int i, j, sum;
 
 	if (argc == 1)
 	{
 		printf("0\n");
+		return (0);
 	}
-
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j]; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (isdigit(argv[i][j]) == 0)
+			if (!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '-'))
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		result += atoi(argv[i]);
+		sum += atoi(argv[i]);
 	}
-	printf("%d\n", result);
+	printf("%d\n", sum);
 	return (0);
 }
-
