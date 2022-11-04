004772e6022eaa85036d88500ee30e0d700ccbce
godcandidate <godcandidate@gmail.com>
2022-11-04 09:06:04 +0000

    create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguments passed
+ *
+ * Return: Always 0.
+ */
+
+int main (int argc, char *argv[])
+{
+	*argv = "";
+	printf("%d\n", argc);
+
+	return (0);
+}
