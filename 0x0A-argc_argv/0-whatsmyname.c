0f124e6b47c7d7218ee12322cf755f43c0c83f7b
godcandidate <godcandidate@gmail.com>
2022-11-04 08:51:55 +0000

    create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints name of program
+ *
+ * Return: Always 0.
+ */
+
+int main (int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+
+	return (0);
+}
