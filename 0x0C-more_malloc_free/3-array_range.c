1f5705fd8e5e5ffa01f0f43bcaf2e654bcf966ec
godcandidate <godcandidate@gmail.com>
2022-11-11 05:14:55 +0000

    update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -6,7 +6,7 @@
  * @min: smallest number to be stored
  * @max: largest number to be stored
  *
- * Return: pointer to the newly created array 
+ * Return: pointer to the newly created array
  */
 int *array_range(int min, int max)
 {
