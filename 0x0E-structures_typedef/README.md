2cfac7db5351a6c6821cc45ee0cc61f670d31471
godcandidate <godcandidate@gmail.com>
2022-11-14 13:31:25 +0000

    create 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+dog.h
+defines a new type struct dog with the elements of name,age and owner
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+66/**
+ * struct dog - dog's description
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: descibes a dog
+ */
+struct my_struct
+{
+	char *name;
+	float age;
+	char *owner;
+};
