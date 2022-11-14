2971a4dee0efef64859e752c146a4ad977b866f2
godcandidate <godcandidate@gmail.com>
2022-11-14 21:01:52 +0000

    update dpg.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,25 @@
-#ifndef DOG_H
-#define DOG_H
-
+#ifndef STRUCTS
+#define STRUCTS
 /**
- * struct dog - contains dog information
+ * struct dog - description for a pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: owner
  *
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Description: hold all data needed for dog
+ * Description: Longer description
  */
-struct dog
+typedef struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-};
-/**
- * dog_t - typedef for struct dog
- */
-typedef struct dog dog_t;
+char *name;
+float age;
+char *owner;
+} dog_t;
+#endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
