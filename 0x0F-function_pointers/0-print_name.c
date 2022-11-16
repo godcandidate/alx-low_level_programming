85eeeec8e3826291654d5c58fafb48f4c48d65a2
godcandidate <godcandidate@gmail.com>
2022-11-16 15:29:10 +0000

    create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - prints name to output
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != 0)
+		f(name);
+}
