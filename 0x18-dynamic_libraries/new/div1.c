d26f81ef5dd7fd36aa10caef35fb4f2a4ff09e6a
godcandidate <godcandidate@gmail.com>
2023-01-20 22:15:14 +0000

    update files

---
+++ 0x18-dynamic_libraries/new/div1.c
@@ -0,0 +1,16 @@
+
+/**
+* div - divides two given integers
+*
+* @a: first integer
+* @b: second integer
+*
+* Return: division of the two integers
+*/
+int div(int a, int b)
+{
+	int ans;
+
+	ans = a / b;
+	return (ans);
+}
---
+++ 0x18-dynamic_libraries/new/sub1.c
@@ -0,0 +1,15 @@
+/**
+* sub - subtract two given integers
+*
+* @a: first integer
+* @b: second integer
+*
+* Return: subtraction of the two integers
+*/
+int sub(int a, int b)
+{
+	int ans;
+
+	ans = a - b;
+	return (ans);
+}
